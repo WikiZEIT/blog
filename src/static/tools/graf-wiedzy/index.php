5344072f2b1292d0124a0f57e423b5b5a5abde05
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-04-27 15:56:28 +0200

    change location of the Knowledge Graph search tool

--- src/static/tools/graf-wiedzy/index.php
+++ src/static/tools/graf-wiedzy/index.php
@@ -30,8 +30,8 @@ if ($query) {
 <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
 <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
 <link rel="shortcut icon" href="/favicon/favicon.ico" />
-<link rel="canonical" href="https://wikizeit.jcubic.pl/graf-wiedzy/" />
-<meta property="og:url" content="https://wikizeit.jcubic.pl/graf-wiedzy/" />
+<link rel="canonical" href="https://wikizeit.jcubic.pl/tools/graf-wiedzy/" />
+<meta property="og:url" content="https://wikizeit.jcubic.pl/tools/graf-wiedzy/" />
 <meta property="og:type" content="website" />
 <meta property="og:locale" content="pl_PL" />
 <meta property="og:site_name" content="WikiZEIT" />
